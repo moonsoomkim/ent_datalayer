d83008a1c7b7d940673a4c24877051d74f16a1d9
Moonsoo <moonsoomkim@users.noreply.github.com>
2016-01-26 16:04:00 -0800

    Create ent_datalayer_en_us.js
    This is an example for the U.S.A.

---
+++ ent_datalayer_en_us.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+var enterpriseDatalayer = {
+   
+  compliance_age : 13,    // Do not track children under 13 so it complies with COPPA.
+  market_info : "en-us",  // Market info where this is relevant.
+  version : "en-us.1"     // Market and Version number.
+  
+}
+</script>
